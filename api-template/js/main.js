e32f234eb87efea59f84fb4922d8b6f15813040e
Ricardo Hernandez <rmhm.photography@gmail.com>
2022-05-08 19:27:25 -0400

    added poke class

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -3,15 +3,27 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`     //choice variable 
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        const potentialPet = new Poke (data.name, data.height, data.weight, data.types,data.sprites.other['official-artwork'])
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
+class Poke {
+  constructor (name, height, weight, types, image) {
+    this.name = name
+    this.height = height
+    this.weight = weight
+    this.types = types
+    this.image = image
+    this.housepet = true
+    this.reason = []
+  }
+}
